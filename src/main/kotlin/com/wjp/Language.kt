b0b9c4d6cdbe66cd8bc26c233733f51b34ef0b86
afeng <1264876271@qq.com>
2019-11-01 16:46:34 +0800

    Initial commit

---
+++ src/main/kotlin/com/wjp/Language.kt
@@ -0,0 +1,13 @@
+package com.wjp
+
+enum class Language(
+    val englishName:String,
+    val codeForApi:String,
+    val suffixForFile:String
+){
+    English("English","en","en"),
+    ChineseSimplified("ChineseSimplified","zh","zh"),
+    ChineseTraditional("ChineseTraditional","cht","zh-rTW"),
+    Japanese("Japanese","jp","ja"),
+    Korean("Korean","kor","ko")
+}
